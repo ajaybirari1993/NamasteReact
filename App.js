8739b12dada77e0cfb07e12085688fa6f0b8c8fa
Ajay Birari <ajaybirari1993@gmail.com>
2026-06-10 21:52:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/*
+	<div id="parent">
+		<div id="child">
+			<h1>I am H1 tag</h1>
+			<h2>I am H2 tag</h2>
+		</div>
+	</div>
+*/
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello Ajay!");
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 Tag"),
+    React.createElement("h2", {}, "I am H2 Tag"),
+  ]),
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
